8189481cbacf383bd15ebf5c170224357a8304e9
RachelMaudlin <73568853+RachelMaudlin@users.noreply.github.com>
2021-02-12 09:25:11 -0500

    scaffolded doc

---
+++ BlueBadgeFinal.WebAPI/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace BlueBadgeFinal.WebAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ BlueBadgeFinal.WebAPI/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace BlueBadgeFinal.WebAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
