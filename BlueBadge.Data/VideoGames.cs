65c7ae2b6fb1ae4c819b649310bf7ebfc2f09b71
shane86920915 <73564809+shane86920915@users.noreply.github.com>
2021-02-14 11:42:04 -0500

    Set some data annotations

--- BlueBadge.Data/VideoGames.cs
+++ BlueBadge.Data/VideoGames.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,19 @@ namespace BlueBadge.Data
 {
     class VideoGames
     {
+      
+
+        [Key]
+        public int GameId { get; set; }
+        [Required]
+        [MinLength(2, ErrorMessage ="There must be a minimum of two characters.")]
+        [MaxLength(50, ErrorMessage ="There are too many characters")]
+        public string GameName { get; set; }
+        [Required]
+        [Range(1, 5, ErrorMessage ="Please choose a rating between 1 and 5.")]
+        public decimal StarRating { get; set; }
+        [Required]
+        public DataType ReleaseYear { get; set; }
+
     }
 }
