428c5decc08c4daa32f823cb9d4913b802cced26
tipowers <tpowers88@outlook.com>
2021-02-16 18:46:41 -0500

    added customer class and propertiess in .data

---
+++ BlueBadge.Data/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueBadge.Data
+{
+    public class Customer
+    {
+        [Key]
+        public int CustomerId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public string ShippingAddress { get; set; }
+    }
+}
