6f151267ccf9f682b3d40bf0a458b675aa049fc5
RachelMaudlin <73568853+RachelMaudlin@users.noreply.github.com>
2021-02-16 20:39:08 -0500

    Order Items Class

---
+++ BlueBadge.Data/OrderItems.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueBadge.Data
+{
+    class OrderItems
+    {
+        [Key]
+        public int OrderId { get; set; }
+        [Required]
+        public int GameId { get; set; }
+        [Required]
+        public int PaymentId { get; set; }
+        [Required]
+        public double Price { get; set; }
+        [Required]
+        public int Quantity { get; set; }
+    }
+}
