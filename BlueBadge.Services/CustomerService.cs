70f6ae2fe480701316e4e648223ec5533fa957e3
tipowers <tpowers88@outlook.com>
2021-02-18 19:02:13 -0500

    Added CustomerCreate method to the CustomerService class

--- BlueBadge.Services/CustomerService.cs
+++ BlueBadge.Services/CustomerService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BlueBadge.Data;
+using BlueBadge.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,30 @@ namespace BlueBadge.Services
 {
     public class CustomerService
     {
+        private readonly Guid _customerId;
+
+        public CustomerService(Guid customerId)
+        {
+            _customerId = customerId;
+        }
+
+        public bool CreateNote(CustomerCreate model)
+        {
+            var entity =
+                new Customer()
+                {
+                    FirstName = model.FirstName,
+                    LastName = model.LastName,
+                    Email = model.Email,
+                    ShippingAddress = model.ShippingAddress,
+                    CreatedUtc = DateTimeOffset.Now
+                };
+
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Customers.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
     }
 }
