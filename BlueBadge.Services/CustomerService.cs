46a73ae9fcd53a7f26684df2cae6b608aa338026
tipowers <tpowers88@outlook.com>
2021-02-18 19:19:35 -0500

    Added GetAllCustomers method to the CustomerService class

--- BlueBadge.Services/CustomerService.cs
+++ BlueBadge.Services/CustomerService.cs
@@ -10,14 +10,9 @@ namespace BlueBadge.Services
 {
     public class CustomerService
     {
-        private readonly Guid _customerId;
+        private readonly int _customerId;
 
-        public CustomerService(Guid customerId)
-        {
-            _customerId = customerId;
-        }
-
-        public bool CreateNote(CustomerCreate model)
+        public bool CreateCustomer(CustomerCreate model)
         {
             var entity =
                 new Customer()
@@ -33,6 +28,29 @@ namespace BlueBadge.Services
             {
                 ctx.Customers.Add(entity);
                 return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public IEnumerable<CustomerList> GetAllCustomers()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                        .Customers
+                        .Where(e => e.CustomerId == _customerId)
+                        .Select(
+                            e =>
+                                new CustomerList
+                                {
+                                    CustomerId = e.CustomerId,
+                                    FirstName = e.FirstName,
+                                    LastName = e.LastName,
+                                    CreatedUtc = e.CreatedUtc
+                                }
+                        );
+
+                return query.ToArray();
             }
         }
     }
